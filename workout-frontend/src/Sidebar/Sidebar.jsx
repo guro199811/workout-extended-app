18f3019fe1d9f14030f90ce8c07493c3b86add2b
guro199811 <guro.sivs@gmail.com>
2024-03-21 23:09:20 +0400

    style: animations for logout window

--- workout-frontend/src/Sidebar/Sidebar.jsx
+++ workout-frontend/src/Sidebar/Sidebar.jsx
@@ -1,20 +1,76 @@
+import { useState, useEffect } from 'react'
 import styles from './Sidebar.module.css';
 
 function Sidebar() {
 
+    const [animation, setAntimation] = useState(false);
+    const [wrapper, setWrapper] = useState(false);
+    const [logout, setLogout] = useState(false);
+
+    useEffect(() => {
+        setAntimation(true);
+    }, [])
+
+    const logoutDiv = () => {
+        setWrapper(true);
+        setTimeout(() => {
+            setLogout(true);
+        }, 150);
+        
+    }
+
+    const logoutNo = () => {
+        setLogout(false);
+        setTimeout(() => {
+            setWrapper(false);
+        }, 300);
+    }
+
+    const logoutYes = () => {
+        setWrapper(false);
+        handleLogout();
+    }
+
     const handleLogout = () => {
         localStorage.removeItem("accessToken");
-        alert("Logged out & Token removed");
-        }
+        window.location.reload();
+    }
 
     return (
-        <div className={styles.sidebar}>
+        <>
+        <div className={`${styles.sidebar}
+            ${animation ? styles.active : ''}`}>
             <a>My Data</a>
             <a>My Goals</a>
             <a>My Schedule</a>
-            <button onClick={() => handleLogout()}>Log-Out</button>
+            <button onClick={() => logoutDiv()}>Log-Out</button>
         </div>
+            {wrapper &&
+            <div className={`'wrapper'
+                ${wrapper ? 'active' : ''}`}>
+                <div className={`${styles.logoutBg}
+                    ${logout ? styles.active : ''}`}>
+                    <div className={`${styles.logoutWindow}
+                    ${logout ? styles.active : ''}`}>
+                        {logout && 
+                        <div>
+                        <h2>Are you sure you want to Log-out? </h2>
+                        <section>
+                            <button onClick={() => logoutYes()}
+                            className={styles.yesBtn}>Yes</button>
+                            <button onClick={() => logoutNo()}
+                            className={styles.noBtn}>No</button>
+                        </section>
+                        </div>
+                        }
+                    </div>
+                </div>
+            </div>
+            }
+        </>
+    
     )
 }
 
 export default Sidebar
+
